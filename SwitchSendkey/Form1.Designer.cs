7da4c5f0323c1a85a29b430f272a852e222f9d6d
UMEGAKI Masahiro <ume@st.rim.or.jp>
2020-06-14 14:18:30 +0900

    プロジェクト ファイルを追加します。

---
+++ SwitchSendkey/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace SwitchSendkey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SW 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SW 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "SW 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 101);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "SW 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 130);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(297, 264);
+            this.textBoxLog.TabIndex = 3;
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(328, 104);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxPort.TabIndex = 4;
+            this.comboBoxPort.SelectedIndexChanged += new System.EventHandler(this.comboBoxPort_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "シリアルポート";
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::SwitchSendkey.Properties.Settings.Default, "SW_4", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox4.Location = new System.Drawing.Point(105, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(204, 19);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Text = global::SwitchSendkey.Properties.Settings.Default.SW_4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::SwitchSendkey.Properties.Settings.Default, "SW_3", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox3.Location = new System.Drawing.Point(105, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(204, 19);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = global::SwitchSendkey.Properties.Settings.Default.SW_3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::SwitchSendkey.Properties.Settings.Default, "SW_2", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox2.Location = new System.Drawing.Point(105, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 19);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = global::SwitchSendkey.Properties.Settings.Default.SW_2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::SwitchSendkey.Properties.Settings.Default, "SW_1", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox1.Location = new System.Drawing.Point(105, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 19);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = global::SwitchSendkey.Properties.Settings.Default.SW_1;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(328, 371);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.Text = "終了";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 473);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Arduino Switch Sendkeys";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ SwitchSendkey/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics.Tracing;
+
+namespace SwitchSendkey
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sio = new SerialPort();
+        string default_port;
+        public Form1()
+        {
+            InitializeComponent();
+            // Create a new SerialPort object with default settings.
+
+            // Allow the user to set the appropriate properties.
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send(textBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send(textBox3.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send(textBox4.Text);
+        }
+
+        private void fire_sendkey(string command)
+        {
+            if (command[0]<0x20)
+            {
+                command = command.Substring(1);
+            }
+            if (command=="Switch 1 ON")
+            {
+                SendKeys.Send(textBox1.Text);
+            }
+            else if (command == "Switch 2 ON")
+            {
+                SendKeys.Send(textBox2.Text);
+            }
+            else if (command == "Switch 3 ON")
+            {
+                SendKeys.Send(textBox3.Text);
+            }
+            else if (command == "Switch 4 ON")
+            {
+                SendKeys.Send(textBox4.Text);
+            }
+        }
+        private void LogString(string str)
+        {
+            textBoxLog.Text += str + "\r\n";
+            textBoxLog.SelectionStart = textBoxLog.Text.Length;
+            textBoxLog.Focus();
+            textBoxLog.ScrollToCaret();
+        } 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!sio.IsOpen) return;
+            if (sio.BytesToRead > 0)
+            {
+                try
+                {
+                    string buff = sio.ReadLine();
+                    LogString(buff);
+                    
+                    fire_sendkey(buff);
+                }
+                catch (TimeoutException timeex)
+                {
+
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sio.Close();
+            Properties.Settings.Default.Save();
+        }
+
+        private void open_sio()
+        {
+            if (sio.IsOpen)
+            {
+                sio.Close();
+            }
+            sio.PortName = default_port;
+            sio.BaudRate = 9600;
+            sio.Parity = Parity.None;
+            sio.DataBits = 8;
+            sio.StopBits = StopBits.One;
+            sio.Handshake = Handshake.None;
+            sio.NewLine = "\r\n";
+
+            // Set the read/write timeouts
+            sio.ReadTimeout = 100;
+            sio.WriteTimeout = 100;
+
+            sio.Open();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // arduino default is 8bit、パリティなし、1ストップビット(SERIAL_8N1)
+            default_port = Properties.Settings.Default.port;
+            bool find_default_port = false;
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                int n = comboBoxPort.Items.Add(port);
+                if (port == default_port)
+                {
+                    comboBoxPort.SelectedIndex = n;
+                    find_default_port = true;
+                }
+            }
+            if (comboBoxPort.Items.Count==0)
+            {
+                MessageBox.Show("シリアルポートが見つかりません", "エラー");
+                Close();
+            }
+            if (!find_default_port)
+            {
+                default_port = comboBoxPort.Items[0].ToString();
+                comboBoxPort.SelectedIndex = 0;
+            }
+            open_sio();
+        }
+
+        private void comboBoxPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            default_port = comboBoxPort.SelectedItem.ToString();
+            open_sio();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
